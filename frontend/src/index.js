d86b137fcd3155a09c107f10308af7e3a2f80a15
Willedge <weihong.will@gmail.com>
2022-08-19 18:24:47 +0800

    first commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Main from "./App";
- 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './App';
+
 ReactDOM.render(
-  <App/>, 
+  <App/>,
   document.getElementById("root")
 );
 
